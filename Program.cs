80940cb346d99e7b5c7d847a99b975c3d4eca2b0
Дячок Микола Володимирович <m.diachok@kt.ua>
2018-10-14 11:05:44 +0300

    add rotate

--- Program.cs
+++ Program.cs
@@ -82,31 +82,80 @@ namespace PrintPDF {
             }
         }
 
-        private static void RotateAndSaveImage (System.Drawing.Bitmap input, int angle) {
-            //Open the source image and create the bitmap for the rotatated image
-            
-            using (Bitmap rotateImage = new Bitmap (input.Width, input.Height)) {
-                //Set the resolution for the rotation image
-                //rotateImage.SetResolution (input.HorizontalResolution, input.VerticalResolution);
-                //Create a graphics object
-                using (Graphics gdi = Graphics.FromImage (rotateImage)) {
-                    //Rotate the image
-                    gdi.TranslateTransform ((float) input.Width / 2, (float) input.Height / 2);
-                    gdi.RotateTransform (angle);
-                    gdi.TranslateTransform (-(float) input.Width / 2, -(float) input.Height / 2);
-                    gdi.DrawImage (input, new System.Drawing.Point (0, 0));
-                }
-
-                //Save to a file
-                rotateImage.Save ("temp1r.bmp");
+        private static void GetPointBounds (PointF[] points,
+            out float xmin, out float xmax,
+            out float ymin, out float ymax) {
+            xmin = points[0].X;
+            xmax = xmin;
+            ymin = points[0].Y;
+            ymax = ymin;
+            foreach (PointF point in points) {
+                if (xmin > point.X) xmin = point.X;
+                if (xmax < point.X) xmax = point.X;
+                if (ymin > point.Y) ymin = point.Y;
+                if (ymax < point.Y) ymax = point.Y;
             }
+        }
+
+        // Return a bitmap rotated around its center.
+        private static Bitmap RotateBitmap (Bitmap bm, float angle) {
+            // Make a Matrix to represent rotation
+            // by this angle.
+            Matrix rotate_at_origin = new Matrix ();
+            rotate_at_origin.Rotate (angle);
+
+            // Rotate the image's corners to see how big
+            // it will be after rotation.
+            PointF[] points = {
+                new PointF (0, 0),
+                new PointF (bm.Width, 0),
+                new PointF (bm.Width, bm.Height),
+                new PointF (0, bm.Height),
+            };
+            rotate_at_origin.TransformPoints (points);
+            float xmin, xmax, ymin, ymax;
+            GetPointBounds (points, out xmin, out xmax,
+                out ymin, out ymax);
+
+            // Make a bitmap to hold the rotated result.
+            int wid = (int) Math.Round (xmax - xmin);
+            int hgt = (int) Math.Round (ymax - ymin);
+            Bitmap result = new Bitmap (wid, hgt);
+
+            // Create the real rotation transformation.
+            Matrix rotate_at_center = new Matrix ();
+            rotate_at_center.RotateAt (angle,
+                new PointF (wid / 2f, hgt / 2f));
+
+            // Draw the image onto the new bitmap rotated.
+            using (Graphics gr = Graphics.FromImage (result)) {
+                // Use smooth image interpolation.
+                gr.InterpolationMode = InterpolationMode.High;
+
+                // Clear with the color in the image's upper left corner.
+                gr.Clear (bm.GetPixel (0, 0));
+
+                //// For debugging. (It's easier to see the background.)
+                //gr.Clear(Color.LightBlue);
+
+                // Set up the transformation to rotate.
+                gr.Transform = rotate_at_center;
+
+                // Draw the image centered on the bitmap.
+                int x = (wid - bm.Width) / 2;
+                int y = (hgt - bm.Height) / 2;
+                gr.DrawImage (bm, x, y);
+            }
+
+            // Return the result bitmap.
+            return result;
         }
 
         static void pd_PrintPage (object sender, PrintPageEventArgs e) {
             var info = new { Width = 100, Height = 50, Margin = 10, Format = ZXing.BarcodeFormat.CODE_128, Code = "12345EAN12345678", ShowCode = false };
             var bitmap = getBarCodeBitmap ();
             AddDownString (bitmap);
-            RotateAndSaveImage(bitmap, 90);
+            bitmap = RotateBitmap (bitmap, 90);
             bitmap.Save ("temp1.bmp");
             e.Graphics.DrawImage (bitmap, 0, 0);
         }
